d2bcb385d839a97b63ca9340e07a928053ed6160
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-21 00:11:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand, :brands
+  attr_reader :brand
 BRANDS = []
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.include?(brand)
-     brand
-   else
-     BRANDS << brand 
-    end
+    if BRANDS.include?(brand)
+    BRANDS << brand 
   end
 end
 
